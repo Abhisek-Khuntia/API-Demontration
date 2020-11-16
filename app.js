7aaba0fa3608aeb17659c2df1fe38ceb4132eb21
Abhisek-Khuntia <abhisek.khuntia16@gmail.com>
2020-11-16 12:34:07 +0530

    Fetch AJAX

---
+++ app.js
@@ -0,0 +1,34 @@
+document.querySelector('.get-jokes').addEventListener('click',getJokes);
+
+
+async function getJokes(e){
+
+    const number= document.querySelector('input[type="number"]').value;
+
+    const xhr= new XMLHttpRequest();
+    xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true);
+    xhr.onload= function(){
+        if(this.status ===200){
+            const response = JSON.parse(this.responseText);
+
+            //console.log(response);
+            let output='';
+            if (response.type=== 'success'){
+                response.value.forEach(function(joke){
+                    output+=`<li>${joke.joke}</li>`
+                });
+
+            }else{
+                output+='Something Went Wrong';
+            }
+            document.querySelector('.jokes').innerHTML=output;
+    }
+}
+    
+    e.preventDefault();
+    
+
+
+    xhr.send()    
+
+}
